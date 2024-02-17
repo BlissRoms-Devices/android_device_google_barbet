6758866b8a23611130826d7f09cefc7c726e00d4
Jackeagle <jackeagle102@gmail.com>
2024-02-17 05:29:09 -0500

    barbet: Update fingerprint/build description from UQ1A.240205.002

--- bliss_barbet.mk
+++ bliss_barbet.mk
@@ -19,6 +19,9 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 5a
 PRODUCT_NAME := bliss_barbet
 
+# Gapps
+TARGET_STOCK_GAPPS := true
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
@@ -26,8 +29,8 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=barbet \
-    PRIVATE_BUILD_DESC="barbet-user 14 UQ1A.240105.002 11129216 release-keys"
+    PRIVATE_BUILD_DESC="barbet-user 14 UQ1A.240205.002 11224170 release-keys"
 
-BUILD_FINGERPRINT := google/barbet/barbet:14/UQ1A.240105.002/11129216:user/release-keys
+BUILD_FINGERPRINT := google/barbet/barbet:14/UQ1A.240205.002/11224170:user/release-keys
 
 $(call inherit-product, vendor/google/barbet/barbet-vendor.mk)
