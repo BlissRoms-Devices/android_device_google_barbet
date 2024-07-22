c7b226564d59ea82152bb46ac0eb7f0fc0f0d213
Jackeagle <jackeagle102@gmail.com>
2024-07-22 09:28:10 -0400

    Update fingerprint/build description from AP2A.240705.004

--- bliss_barbet.mk
+++ bliss_barbet.mk
@@ -28,8 +28,8 @@ TARGET_STOCK_GAPPS:= true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=barbet \
-    PRIVATE_BUILD_DESC="barbet-user 14 AP1A.240505.004 11583682 release-keys"
+    PRIVATE_BUILD_DESC="barbet-user 14 AP2A.240705.004 11875680 release-keys"
 
-BUILD_FINGERPRINT := google/barbet/barbet:14/AP1A.240505.004/11583682:user/release-keys
+BUILD_FINGERPRINT := google/barbet/barbet:14/AP2A.240705.004/11875680:user/release-keys
 
 $(call inherit-product, vendor/google/barbet/barbet-vendor.mk)
